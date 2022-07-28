16fede266bf4b727bdfe43015c4b4e5a16cd63e6
ksuna123 <110160190+ksuna123@users.noreply.github.com>
2022-07-28 13:33:43 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?  
+
+$path=$_SERVER['DOCUMENT_ROOT'];
+require_once "$path/sys/db.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<? include_once "$path/private/head.php"; ?>
+<body>
+	<div class="cont">
+		<? include_once "$path/private/header.php"; ?>
+
+		<main>
+			<div class="wrapper">
+			<div class="delivery">
+				<div class="delivery-title">Доставка курьером</div>
+				<div class="delivery-subtitle">Бесплатно за 24 часа</div>
+			</div>
+			</div>
+		</main>
+
+		<? include_once "$path/private/footer.php"; ?>
+		
+	</div>
+	<script>
+		
+			
+	</script>
+</body>
+</html>
